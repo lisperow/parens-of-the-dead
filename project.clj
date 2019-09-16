211d11e19ec17821beee9ce2284a7698a8231e41
lisperow <lisperow@yandex.ru>
2019-09-16 23:40:37 +0300

    fix

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :jvm-opts ["-XX:MaxPermSize=256m"]
-  :dependencies [[org.clojure/clojure ""]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [http-kit "2.3.0"]]
   :profiles {:dev {:plugins []
                    :dependencies []
                    :sourse-paths ["dev"]}})
